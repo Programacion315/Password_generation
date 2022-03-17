9080099e427fffd28343250081c62efd9ce30631
Programacion <jorgeluishuergo88@gmail.com>
2022-03-16 21:58:30 -0500

    Agregado funcionalidad de botoens

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 (function(){
-
+    
+    //Variables y objetos generales
     var app = document.querySelector("#app");
     var inputCaracteres = document.querySelector("#number-characters");
 
@@ -14,7 +15,30 @@
     var caracteres = {
         numeros: '0 1 2 3 4 5 6 7 8 9',
         simbolos: '! @ # % $ * - _ { } [] ? . , ;',
-        mayusculas: ''
+        mayusculas: 'A B C D E F G H I J K L M N R S T O P Q X Y Z',
+        minusculs:  'a b c d e f g h i j k l m n r s t o p q x y z'
     }
+
+    //Eventos
+
+
+    app.addEventListener('submit', function(e){
+
+        e.preventDefault(); //Previene el comportamiento por defecto
+    });
+
+    app.elements.namedItem('btn-plus-one').addEventListener('click',function(){
+        configuracion.caracteres++;
+        inputCaracteres.value = configuracion.caracteres;
+    });
+
+    app.elements.namedItem('btn-minus-one').addEventListener('click',function(){
+
+        if(configuracion.caracteres > 1){
+            configuracion.caracteres--;
+            inputCaracteres.value = configuracion.caracteres;
+        }
+        
+    });
    
 }())
